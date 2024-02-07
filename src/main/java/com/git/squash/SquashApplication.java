2e3c6b5560cc07305f3b282580fcc4f72f055588
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2024-02-07 03:33:12 +0000

    Initial commit

---
+++ src/main/java/com/git/squash/SquashApplication.java
@@ -0,0 +1,13 @@
+package com.git.squash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/squash/SquashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.squash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
